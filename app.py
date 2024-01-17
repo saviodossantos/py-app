7e4aa06c2546fb152c52a8ad34a4a4cc237d5288
saviodossantos <savio.dossantos@outlook.com>
2024-01-17 09:01:15 -0300

    condicionais e funcoes

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
 ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 ─██████████████─██████████████─██████████████───██████████████─████████████████──────██████████████─████████──████████─██████████████─████████████████───██████████████─██████████████─██████████████─
@@ -19,5 +21,17 @@ print('2. Listar restaurantes')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}.')
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+def finalizar_app():
+    os.system('cls')
+    print('Saindo...\n')
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    finalizar_app()
